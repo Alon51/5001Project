3ef0ebc6e318d953f37715a62280758c94fe2d46
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-02-13 15:11:11 -0500

    Prompt on Console

---
+++ GameProject/Assets/Scripts/DesktopController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DesktopController : MonoBehaviour {
+
+	public Text prompt; //text to display
+
+	// Use this for initialization
+	void Start () {
+		prompt.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			//show interact text
+			prompt.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		prompt.enabled = false;
+	}
+}

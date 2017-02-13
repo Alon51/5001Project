9fa50c5cb6367e0aea201922a3f296708d5c1789
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-02-13 15:26:56 -0500

    Robo Jump

--- GameProject/Assets/Scripts/Player/PlayerMovement.cs
+++ GameProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 
-public float moveSpeed; // how fast the player moves 
-public float jumpSpeed; // how high player jumps
+	public float moveSpeed; // how fast the player moves 
+	public float jumpSpeed; // how high player jumps
 
-private Rigidbody2D myRigidBody; // rigid body used for moving and jumping
+	public bool isGrounded; // know if player is on ground
+	public bool isJumping; // know if player is jumping
 
-private Animator anim;
+	//variables for checking if player is on ground or not
+	public Transform groundCheck;
+	public float groundCheckRadius; // radius of ground check space
+	public LayerMask whatIsGround;
+
+	private Rigidbody2D myRigidBody; // rigid body used for moving and jumping
+
+	private Animator anim;
 
 //Animation States 
 	const int STATE_IDLE = 0;
@@ -38,7 +46,7 @@ private Animator anim;
 	// Update is called once per frame
 	void Update () {
 
-		//isGrounded = Physics2D.OverlapCircle (groundCheck.position,groundCheckRadius,whatIsGround);
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position,groundCheckRadius,whatIsGround);
 
 		//HorizontaL input is either 0(no input), 1(going right), or -1(going left)
 
@@ -60,6 +68,24 @@ private Animator anim;
 			myRigidBody.velocity = new Vector3 (0f, myRigidBody.velocity.y, 0f);
 			changeState(STATE_IDLE);
 		}
+
+
+		// checking jump input(space or up)
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			// put jumpSpeed in y to move up by moveSpeed
+
+			myRigidBody.velocity = new Vector3 (myRigidBody.velocity.x, jumpSpeed, 0f);
+			isJumping = true;
+
+		}
+		// if on the ground, set falling and jumping to false
+		else if(isGrounded){ 
+			
+			isJumping = false;
+
+		}
+
+
 	}
 
 		void changeState(int state){

c39b2e5416af2de22f9fc90573a8590397399e6c
mike200385 <mikewilliams76901@gmail.com>
2017-02-13 14:42:49 -0500

    Imports from Mike's project
    Has lots of sprites/scripts/etc

---
+++ GameProject/Assets/Scripts/Puzzle3/CompletionScriptThree.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CompletionScriptThree : MonoBehaviour {
+
+	public reactToInt intSuccess;
+	public reactToBreakOne breakOneSuccess;
+	public reactToBreakTwo breakTwoSuccess;
+	public reactToBreakThree breakThreeSuccess;
+	public reactToSwitchCondition switchSuccess;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
---
+++ GameProject/Assets/Scripts/TileReaction/TileDrag.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileDrag : MonoBehaviour {
+
+	float x;
+	float y;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+	}
+	void OnMouseDrag(){
+		transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (x, y, 10.0f));
+	}
+
+}

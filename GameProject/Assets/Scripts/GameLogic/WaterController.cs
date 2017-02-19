7cba50ca0f5eb4e027b646afe6de413887bf3faf
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-02-19 18:39:06 -0500

    Testing Water Level
    Added the ability for the player to raise the water level with the ‘r’
    key, and lower with ‘l’ key for testing

---
+++ GameProject/Assets/Scripts/GameLogic/WaterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour {
+
+	public GameObject Water;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y+1, 0); // move water upward
+		}
+		if (Input.GetKeyDown (KeyCode.L)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y-1, 0); // move water downward
+		}
+	}
+}

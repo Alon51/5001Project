de33aa79bb2b63c93d9e669a5160d94ddaa80b90
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-02-20 01:03:54 -0500

    Teleporter Additions
    Pressing x on teleports send you to the respective scene.

---
+++ GameProject/Assets/Scripts/GameLogic/TeleporterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TeleporterController : MonoBehaviour {
+
+	private bool inArea = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		if (inArea) {
+//			if (Input.GetKeyDown ("x")) {
+//				SceneManager.LoadScene ("ArrayLevel");
+//			}
+//		}
+	}
+
+	void OnCollisionStay2D(Collision2D other){
+		if (other.gameObject.tag == "Player") {
+			//show interact text
+			if (Input.GetKeyDown ("x")) {
+				//send it the tag, which is also the level name
+				GlobalController.Instance.changeScene (this.tag); 
+			}
+			//inArea = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		//inArea = false;
+	}
+}

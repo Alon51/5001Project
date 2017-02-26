74f0ef77dde674c2ba7a098943ad645690b2c953
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-02-25 21:49:43 -0500

    Click/Drag Fix

--- GameProject/Assets/Scripts/TileReaction/TileDrag.cs
+++ GameProject/Assets/Scripts/TileReaction/TileDrag.cs
@@ -10,16 +10,19 @@ public class TileDrag : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		x = Input.mousePosition.x;
-		y = Input.mousePosition.y;
-		z = 200.0f;
+		
 	}
 	void OnMouseDrag(){
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+		// take the object's Z and correctly adjust it by subtracting the camera's Z
+		// this makes up for the odd z translation that sometimes occurs
+		z = transform.position.z - testCam.transform.position.z;
 		transform.position = testCam.ScreenToWorldPoint(new Vector3 (x, y, z));
 	}
 

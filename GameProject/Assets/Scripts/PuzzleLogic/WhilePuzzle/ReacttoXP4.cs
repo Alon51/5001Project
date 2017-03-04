c89515d5b189a547aa52e1f60d81e6391b5bd00a
mike200385 <mikewilliams76901@gmail.com>
2017-03-04 12:49:12 -0500

    Tiles
    Also some messing around with Reset button

--- GameProject/Assets/Scripts/PuzzleLogic/WhilePuzzle/ReacttoXP4.cs
+++ GameProject/Assets/Scripts/PuzzleLogic/WhilePuzzle/ReacttoXP4.cs
@@ -5,7 +5,8 @@ public class ReacttoXP4 : MonoBehaviour {
 
 	public GameObject completedTile;
 	public bool success;
-
+	public TileDrag activate;
+	private float tossThis = 1000.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +15,20 @@ public class ReacttoXP4 : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "x" && !success) {
 			SpriteRenderer.Instantiate (completedTile, this.transform.position, Quaternion.identity);
-			other.gameObject.SetActive (false);
 			success = true;
+			activate.canIDragit = false;
+			other.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, 
+				this.transform.position.y + tossThis, 0);
 		}
 	}
+
+	void Update () {
+		if (Input.GetKeyDown("r")) {
+			slotReset ();
+		}
+	}
+
+	void slotReset(){
+	}
+		
 }

3471dacda57b3ab6f8d755b0b51f44edacff4204
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-03-07 00:20:58 -0500

    Arithmetic Level Additions

--- GameProject/Assets/Scripts/JITScripts/JITScript.cs
+++ GameProject/Assets/Scripts/JITScripts/JITScript.cs
@@ -7,6 +7,7 @@ public class JITScript : MonoBehaviour {
 	// JUST IN TIME INSTRUCTIONS
 
 	public Text wordDisplay;
+	public string jitName;
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +21,31 @@ public class JITScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Player") {
-			switch (this.tag) {
+			switch (this.jitName) {
 
+			case "ArrayBriefing":
+				wordDisplay.text = "There are more scientists to be saved using Arrays! \n" +
+					"An array is a list of elements of the same type. \n " +
+					"Arrays count their elements starting at 0! Remember that! " +
+					"Arrays can be accessed like this: array[num] Where num is a number.";
+				Time.timeScale = 0.0f;
+				Destroy (this.gameObject);
+				break;
+			case "ArrayChallenge1":
+				wordDisplay.text = "It seems some of the holo-platforms are diabled, which means you can't get across!" +
+					"\nUse this terminal to try and fix them. Turn on the First, Third, and Last platforms." +
+					" Watch out for weird array indexes";
+				Time.timeScale = 0.0f;
+				Destroy (this.gameObject);
+				break;
+			case "ArrayChallenge2":
+				wordDisplay.text = "The counter-weight platform is down, but you need to get across. " +
+					"Hmm, see those weighted boxed up there, they seem linked ot this terminal! " +
+					"It seems that you need a weight of 14 to align the counter-weight platform. " +
+					"Put your coding skills to the test and drop a total of 14lbs on the platform.";
+				Time.timeScale = 0.0f;
+				Destroy (this.gameObject);
+				break;
 			case "Scientist1":
 				wordDisplay.text = "Thanks for saving me! I'll help you regain control of the ship!";
 				Time.timeScale = 0.0f;
---
+++ GameProject/Assets/Scripts/PuzzleLogic/LoopPuzzles/ArithCompletionCheck.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ArithCompletionCheck : MonoBehaviour {
+
+	// LEVEL MANAGER FOR THE ARRAY LEVEL
+
+	public ArrayReaction check1, check2, check3, check4,check5;
+
+	public GameObject[] arrayTiles; // the tiles that will be dragged
+	public GameObject[] replacementTiles;
+	public List<Vector3> arrayTilePositions; // their positions, used for resetting challenge
+
+	public bool puzzleFinished;
+	// Use this for initialization
+	void Start () {
+		arrayTiles = GameObject.FindGameObjectsWithTag ("ArrayTile");
+		puzzleFinished = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if all 3 spots are filled
+		if (check1.success && check2.success && check3.success && check4.success && check5.success) {
+			//drop the platforms
+			GlobalController.Instance.toggleCamera();
+			makePlatformsVisible ();
+			//reset everything for the next use	
+			GlobalController.Instance.resetBoxBools();
+			resetTiles ();
+			resetSlots ();
+
+			//resetCheckValues ();
+		}	
+
+	}
+
+	public void resetCheckValues(){
+		check1.resetSuccessBool ();
+		check2.resetSuccessBool ();
+		check3.resetSuccessBool ();
+		check4.resetSuccessBool ();
+		check5.resetSuccessBool ();
+	}
+
+	public void makePlatformsVisible(){
+		foreach (GameObject temp in arrayTiles) {
+			// if this tile was used as part of the sum
+			if (temp.GetComponent<ArrayTileController> ().isUsed) { 
+				//drop it's corresponding box
+				temp.GetComponent<ArrayTileController>().connectedPlatform.SetVisibleAndActive();
+				temp.GetComponent<ArrayTileController> ().resetUsed ();
+			}
+
+		}
+		//resetCheckValues ();
+	}
+	//reset slots to empty
+	public void resetSlots(){
+		replacementTiles = GameObject.FindGameObjectsWithTag ("ReplaceTile");
+
+		foreach (GameObject repTile in replacementTiles) {
+			Destroy (repTile);
+		}
+		//THIS LINE ALSO CAUSES PROBLEMS
+		resetCheckValues ();
+	}
+
+	// reset tiles to active and in original position
+	public void resetTiles(){
+		for (int i = 0; i < arrayTiles.Length; i++) {
+			//set to initial position
+			arrayTiles[i].GetComponent<TileDrag>().onReset();
+			arrayTiles [i].GetComponent<ArrayTileController> ().resetUsed (); // change bool to false;
+			// THIS LINE MESSES UP THE RESET
+			//arrayTiles [i].GetComponent<BoxCollider2D>().enabled = true;
+
+		}
+
+	}
+
+}
